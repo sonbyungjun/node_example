a80c01e01eb61c748dce057fa5c04cc93227a73d
Byungjun Son <saint2030@naver.com>
2019-08-26 22:33:31 +0900

    3 day

---
+++ learn-mongoose/routes/users.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../schemas/user');
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  User.find()
+  .then((users) => {
+    res.json(users);
+  })
+  .catch((err) => {
+    console.error(err)
+    next(err)
+  })
+});
+
+router.post('/', (req, res, next) => {
+  const user = new User({
+    name: req.body.name,
+    age: req.body.age,
+    married: req.body.married
+  })
+  user.save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    })
+});
+
+module.exports = router;
