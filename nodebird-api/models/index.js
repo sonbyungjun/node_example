bd979ad312c501b1a52b0e3d89e2ca55564389a7
Byungjun Son <saint2030@naver.com>
2019-10-01 10:17:57 +0900

    validate

--- nodebird-api/models/index.js
+++ nodebird-api/models/index.js
@@ -16,6 +16,7 @@ db.Domain = require('./domain')(sequelize, Sequelize);
 
 db.User.hasMany(db.Post);
 db.Post.belongsTo(db.User);
+
 db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
 db.Hashtag.belongsToMany(db.Post, { through: 'PostHashtag' });
 db.User.belongsToMany(db.User, {
